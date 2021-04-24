821f9bb75385cb7af77b1afa0c7f498aae55a18d
Ian Fitzpatrick <itfitzpatrick@gmail.com>
2021-04-24 13:05:05 -0700

    feat: initialize repo

---
+++ graphql-doc.el
@@ -0,0 +1,25 @@
+;;; graphql-doc.el --- GraphQL Doc -*- lexical-binding: t -*-
+
+;; Copyright (c) 2021 Ian Fitzpatrick <itfitzpatrick@gmail.com>
+
+;; URL: https://github.com/ifitzpatrick/graphql-doc
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Author: Ian Fitzpatrick
+;; Created: April 25, 2021
+;; Package-Version: 0.0.1
+;; Package-Requires: ((emacs "25.1") (request "0.3.2") (promise "1.1"))
+
+;;; Commentary:
+;; GraphQL Documentation explorer
+
+;;; Code:
+
+(message "test")
+
+(provide 'graphql-doc)
+
+;;; graphql-doc.el ends here
