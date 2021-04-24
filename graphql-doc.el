7bfb4a6364a0bf8f678e5d46ff7e2f8d1b39272e
Ian Fitzpatrick <itfitzpatrick@gmail.com>
2021-04-24 13:37:50 -0700

    feat(request): Request introspection queries from endpoints

--- graphql-doc.el
+++ graphql-doc.el
@@ -18,7 +18,139 @@
 
 ;;; Code:
 
-(message "test")
+(require 'cl-lib)
+(require 'promise)
+(require 'request)
+
+(defvar graphql-doc--introspection-query
+"query IntrospectionQuery {
+  __schema {
+    queryType { name }
+    mutationType { name }
+    subscriptionType { name }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+
+fragment FullType on __Type {
+  kind
+  name
+  description
+  
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+
+fragment InputValue on __InputValue {
+  name
+  description
+  type { ...TypeRef }
+  defaultValue
+}
+
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}")
+
+(defun graphql-doc--request (url data)
+  "Helper function to make http requests to graphql endpoints."
+  (promise-new
+   (lambda (resolve reject)
+     (request
+       url
+       :type "POST"
+       :parser 'json-read
+       :data
+       data
+       :error
+       (cl-function
+        (lambda (&key response &allow-other-keys)
+          (funcall reject (request-response-data response))))
+       :success
+       (cl-function
+        (lambda (&key response &allow-other-keys)
+          (funcall resolve (request-response-data response))))))))
+
+(defvar-local graphql-doc--introspection-results nil)
+
+(defun graphql-doc--request-introspection (api)
+  "Request introspection query from API."
+  (promise-chain
+      (graphql-doc--request
+       (plist-get api :url)
+       (append
+        (plist-get api :data)
+        `(("variables" . "")
+          ("query" . ,graphql-doc--introspection-query))))
+    (then (lambda (data)
+            (setq-local graphql-doc--introspection-results data)))
+    (promise-catch (lambda (data)
+                     (message "error: %s" data)))))
 
 (provide 'graphql-doc)
 
